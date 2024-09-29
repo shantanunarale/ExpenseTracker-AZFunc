bd890fae86a5e1b7d42c08a2170ff7ec1df0d573
shantanunarale <shantanunarale062@gmail.com>
2024-09-30 03:20:05 +0530

    Dev Complete

--- function_blueprints.py
+++ function_blueprints.py
@@ -1,7 +1,8 @@
 import azure.functions as func
 import logging
 import json
-import datetime
+from datetime import datetime
+from zoneinfo import ZoneInfo
 import uuid
 
 bp = func.Blueprint()
@@ -16,7 +17,7 @@ def fn_logExpenseEvents(req: func.HttpRequest, expbody: func.Out[str]):
         
         {
             \"Category\":\"<<Expense Category>>\",
-            \"Payment Method\":\"<<Payment Method>>\",
+            \"PaymentMethod\":\"<<Payment Method>>\",
             \"Bank\":\"<<Bank>>\",
             \"Amount\":\"<<Amount>>\",
             \"Expense\":\"<<Expense>>\"
@@ -26,9 +27,13 @@ def fn_logExpenseEvents(req: func.HttpRequest, expbody: func.Out[str]):
         return func.HttpResponse(body=response_body, status_code=200)
     if req.method == "POST":
         payload = req.get_json()
-        today = datetime.datetime.now()
+        today = datetime.now(tz=ZoneInfo('UTC'))
+        today = today.astimezone(tz=ZoneInfo("Asia/Calcutta"))
         payload["PartitionKey"] = today.strftime("%B-%Y")
         payload["RowKey"] = today.strftime("%d") + "-" + str(uuid.uuid4())
+        if "Payment Method" in payload.keys():
+            payment_method = payload.pop("Payment Method")
+            payload["PaymentMethod"] = payment_method
         resp_body = ""
         resp_code = 500
         try:
